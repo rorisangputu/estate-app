fd832248f1efde41961f2ae5cb4f97115c6a4b5c
Rorisang Putu <rorisangputu@gmail.com>
2024-12-03 23:35:13 +0200

    routing configuring

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from '../src/pages/Home'
 function App() {
 
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
