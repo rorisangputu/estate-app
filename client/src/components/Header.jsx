aa0f0a6b0f82e4a7c79a4967d89be398222ce908
Rorisang Putu <rorisangputu@gmail.com>
2024-12-04 00:22:51 +0200

    installing react-icons

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const Header = () => {
     return (
-        <div>Header</div>
+        <header className='bg-slate-200 shadow-md '>
+            <div className='w-[90%] mx-auto flex justify-between pt-3 pb-3'>
+                <div>
+                    <h1 className='font-bold text-sm sm:text-xl flex flex-wrap gap-1'>
+                        <span className='text-slate-500'>Bright</span>
+                        <span className='text-slate-700'>Estates</span>
+                    </h1>
+                </div>
+                <div>
+                    <form action="" className='bg-slate-100 p-3 rounded-lg'>
+                        <input type="text" className='bg-transparent' placeholder='Search' name="" id="" />
+                        
+                    </form>
+                </div>
+            </div>
+        </header>
     )
 }
 
