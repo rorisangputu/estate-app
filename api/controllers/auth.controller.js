6b51aff6d6dc7e5a792bf3dafc8124453b5ad3dc
Rorisang Putu <rorisangputu@gmail.com>
2024-12-04 12:19:54 +0200

    adding password encryption

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,17 +1,22 @@
 import User from '../models/user.model.js';
+import bcrypt from 'bcryptjs'
 
 export const signUp = async (req, res) => {
-    //Destructering request body
+    //Destructuring request body
     const { username, email, password } = req.body;
 
+    //Encrypting password and adding salts
+    const cryptPass = bcrypt.hashSync(password, 10);
+
     //Creating new user instance and saving request body variables to model
-    const newUser = new User({ username, email, password })
+    //Adding encrypted password as password
+    const newUser = new User({ username, email, password: cryptPass })
 
     //TryCatch saving user to db
     try {
         //Saving new user instance and creating a user in db
-        const savedUser = await newUser.save();
-        //return succesful request status code 
+        await newUser.save();
+        //Return succesful request status code 
         res.status(201).json('User created succesfully!');
     } catch (error) {
         const errors = Object.values(error.errors).map(err => err.message);
