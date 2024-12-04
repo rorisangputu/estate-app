c2d01c0fddea1e5dc466c5f62212358c8f45b2f2
Rorisang Putu <rorisangputu@gmail.com>
2024-12-04 20:46:33 +0200

    configuring proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,8 +5,11 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   server: {
     proxy: {
-      '/api' : 'http://localhost:3000'
-    }
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false
+      },
+    },
   },
   plugins: [react()],
 })
