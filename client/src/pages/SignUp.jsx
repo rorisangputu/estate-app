3d40458e46877ab1f76a26abae6211d05d37ee03
Rorisang Putu <rorisangputu@gmail.com>
2024-12-04 19:59:04 +0200

    adding state handler

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+    const [formData, setFormData] = useState({})
+
+    //Handles changing state of inputs and stores to Form state variable
+    const handleChange = (e) => {
+        e.preventDefault();
+        setFormData({
+            ...formData,
+            [e.target.id]: e.target.value,
+        });
+    }
+
+    //console.log(formData)
+
     return (
         <div className="">
             <div className="max-w-lg mx-auto p-3">
                 <h1 className="text-3xl text-center font-semibold my-7" > Sign Up</h1>
                 <form action="" className="flex flex-col mb-7 gap-3  mx-auto">
-                    <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" />
-                    <input type="text" placeholder="Email" className="border p-3 rounded-lg" id="email" />
-                    <input type="text" placeholder="Password" className="border p-3 rounded-lg" id="password" />
+                    <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" onChange={handleChange} />
+                    <input type="text" placeholder="Email" className="border p-3 rounded-lg" id="email" onChange={handleChange} />
+                    <input type="text" placeholder="Password" className="border p-3 rounded-lg" id="password" onChange={handleChange} />
                     <button className="bg-slate-700 text-white p-3 rounded-lg uppercase 
                         hover:opacity-95 cursor-pointer disabled:opacity-80"
                     >
