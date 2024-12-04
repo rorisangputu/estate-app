08d1f3790f7a25980483884508800c4f911d7c26
Rorisang Putu <rorisangputu@gmail.com>
2024-12-04 11:15:04 +0200

    adding new api routes

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv'
+import userRoutes from './routes/user.route.js'
 
 dotenv.config();
 
@@ -14,9 +15,10 @@ const connect = async () => {
         console.log(error);
     }
 }
-    
-
 const app = express();
+
+app.use('/api/user', userRoutes )
+
 app.listen(3000, () => {
     connect();
     console.log('Running on PORT 3000')
