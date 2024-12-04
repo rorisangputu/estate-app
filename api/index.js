926c6b7900a512b28ac96f8d6a554483890a39cd
Rorisang Putu <rorisangputu@gmail.com>
2024-12-04 09:56:34 +0200

    creating db connection

--- api/index.js
+++ api/index.js
@@ -1,6 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+
+dotenv.config();
+
+//db connection
+const connect = async () => {
+    mongoose.set('strictQuery', true);
+    try {
+        await mongoose.connect(process.env.MONGO_CON);
+        console.log('Connected to Database')
+    } catch (error) {
+        console.log(error);
+    }
+}
+    
 
 const app = express();
 app.listen(3000, () => {
+    connect();
     console.log('Running on PORT 3000')
+    
 })
