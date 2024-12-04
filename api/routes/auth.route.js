820b89d685c246702e7731eacccde1be94bf5b90
Rorisang Putu <rorisangputu@gmail.com>
2024-12-04 11:19:06 +0200

    creating auth routes

---
+++ api/routes/auth.route.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { signUp } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post('/sign-up', signUp)
+
+export default router;
