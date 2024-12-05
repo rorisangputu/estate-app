c17bd98dd64948690c139b166558bf1adcd96d78
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 00:52:44 +0200

    sign in

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -52,7 +52,6 @@ const SignIn = () => {
             <div className="max-w-lg mx-auto p-3">
                 <h1 className="text-3xl text-center font-semibold my-7" > Sign In</h1>
                 <form onSubmit={handleSubmit} action="" className="flex flex-col mb-7 gap-3  mx-auto">
-                    <input type="text" placeholder="Username" className="border p-3 rounded-lg" id="username" onChange={handleChange} />
                     <input type="text" placeholder="Email" className="border p-3 rounded-lg" id="email" onChange={handleChange} />
                     <input type="text" placeholder="Password" className="border p-3 rounded-lg" id="password" onChange={handleChange} />
                     <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase 
@@ -66,8 +65,9 @@ const SignIn = () => {
                         Sign in with Google
                     </button>
                 </form>
-                <p>Don&apos;t have an account? <span className="text-blue-500 cursor-pointer hover:underline"> <Link disabled={loading} to={'/sign-up'}>Sign Up</Link></span></p>
-                {error ? <p className='text-red-600 mt-5'>{error.message}</p> : <></>}
+                <p>Create an account: <span className="text-blue-500 cursor-pointer hover:underline"> <Link disabled={loading} to={'/sign-up'}>Sign Up</Link></span></p>
+                {error && <p className='text-red-600 mt-5'>{error.message}</p>}
+
             </div >
         </div >
     )
