1aeaa176fd0c28af18999603c48cbf59254dc6bf
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 01:10:25 +0200

    configuring initial state and creating slice

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false
+}
+
+const userSlice = createSlice({
+
+    name: 'user',
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+        }
+    }
+    
+})
