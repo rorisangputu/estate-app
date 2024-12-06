e0835d445c90040f4e35622e159174e5c45268bc
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 02:37:01 +0200

    removing .env from git

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -1,8 +1,16 @@
 
 
 const OAuth = () => {
+
+    const handleGoogleClick = async () => {
+        try {
+            
+        } catch (error) {
+            console.log('Could not sign in with Google', error)
+        }
+    }
     return (
-        <button className="uppercase bg-red-700 p-3 text-white rounded-lg cursor-pointer
+        <button onClick={handleGoogleClick} type="button" className="uppercase bg-red-700 p-3 text-white rounded-lg cursor-pointer
                         hover:opacity-95 disabled:opacity-80"
         >
             Sign in with Google
