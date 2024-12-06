82f31ae7cee5e0d9d86a535e224bfe9a7d52baf8
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 23:36:57 +0200

    adding verify method to route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express, { Router } from "express";
 import { test, updateUserInfo } from "../controllers/user.controller.js";
+import { verifyToken } from "../utils/verifyUser.js";
 const router = Router();
 
 router.get('/', test);
-router.put('/update', updateUserInfo);
+router.put('/update', verifyToken, updateUserInfo);
 
 export default router;
