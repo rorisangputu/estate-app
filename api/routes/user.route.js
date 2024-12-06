a71e6478f4c6cf53d36ae332f8d50b9550de1aac
Rorisang Putu <rorisangputu@gmail.com>
2024-12-07 01:32:33 +0200

    creating delete user route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import {  updateUserInfo } from "../controllers/user.controller.js";
+import {  deleteUserInfo, updateUserInfo } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = Router();
 
 
 router.post('/update/:id', verifyToken, updateUserInfo);
+router.post('/delete/:id', verifyToken, deleteUserInfo);
 
 export default router;
