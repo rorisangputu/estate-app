8fdd71d03a91f08d4201dfffc46ee5616d7513ca
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 03:35:11 +0200

    on successful google signin/signup user is navigated to home

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -2,9 +2,11 @@ import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
 import { app } from '../firebase';
 import { useDispatch } from 'react-redux'
 import { signInSuccess } from '../redux/user/userSlice.js'
+import { useNavigate } from 'react-router-dom';
 
 const OAuth = () => {
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     const handleGoogleClick = async () => {
         try {
@@ -23,6 +25,7 @@ const OAuth = () => {
 
             const data = await res.json();
             dispatch(signInSuccess(data))
+            navigate('/');
         } catch (error) {
             console.log('Could not sign in with Google', error)
         }
