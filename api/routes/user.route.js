835421f01140c40352e100e492df9511aa254445
Rorisang Putu <rorisangputu@gmail.com>
2024-12-07 00:28:46 +0200

    added id to url

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,9 @@
 import express, { Router } from "express";
-import { test, updateUserInfo } from "../controllers/user.controller.js";
+import {  updateUserInfo } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = Router();
 
-router.get('/', test);
-router.put('/update', verifyToken, updateUserInfo);
+
+router.post('/update/:id', verifyToken, updateUserInfo);
 
 export default router;
