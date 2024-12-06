ee32df07393c0a8762b4cea89dfcfadb05424923
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 02:19:41 +0200

    creating google sign-up/login buttom

---
+++ client/src/components/OAuth.jsx
@@ -0,0 +1,13 @@
+
+
+const OAuth = () => {
+    return (
+        <button className="uppercase bg-red-700 p-3 text-white rounded-lg cursor-pointer
+                        hover:opacity-95 disabled:opacity-80"
+        >
+            Sign in with Google
+        </button>
+    )
+}
+
+export default OAuth
