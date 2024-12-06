b1ea15afd4d682e229d412f9a5aa474a21391919
Rorisang Putu <rorisangputu@gmail.com>
2024-12-07 00:38:09 +0200

    importing error handler

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from '../models/user.model.js'
+import { errorHandler } from '../utils/error.js';
 
 export const updateUserInfo = async (req, res, next) => {
     
