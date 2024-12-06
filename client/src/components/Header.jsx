f98c56d75e09f3ff640757eb75b98451a54a2e8d
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 03:49:59 +0200

    adding current users avatar to header when logged in

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import { FaSearch } from 'react-icons/fa'
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+    const { currentUser } = useSelector(state => state.user);
     return (
         <header className='bg-slate-200 shadow-md '>
             <div className='w-[90%] mx-auto flex justify-between items-center pt-3 pb-3'>
@@ -21,10 +23,26 @@ const Header = () => {
                     </form>
                 </div>
                 <div>
-                    <ul className='justify-between hidden md:flex gap-4'>
+                    <ul className='justify-between hidden md:flex gap-4 items-center'>
                         {links.map((link, i) => (
                             <Link to={link.href} className='text-slate-700 hover:underline cursor-pointer' key={i}>{link.link}</Link>
                         ))}
+                        {currentUser ?
+                            (
+                                <Link to={'/profile'}>
+                                    <img className='h-10 w-10 rounded-full object-cover' src={currentUser.avatar} alt="profile " />
+                                </Link>
+                            )
+                            :
+                            (
+                                <Link
+                                    to={'/sign-in'}
+                                    className='text-slate-700 hover:underline cursor-pointer'
+                                >
+                                    Sign In
+                                </Link>
+                            )
+                        }
                     </ul>
                     <div className='md:hidden'>
                         <GiHamburgerMenu className='h-7 w-7 cursor-pointer text-slate-700' />
@@ -50,8 +68,5 @@ const links = [
         link: 'Profile',
         href: '/profile'
     },
-    {
-        link: 'Sign In',
-        href: '/sign-in'
-    },
+
 ]
