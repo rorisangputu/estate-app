23b59b53e4f162289b3c48d4b1620d60ec3f4a31
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 23:47:30 +0200

    updating password if password was chnged

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,8 +1,20 @@
-export const test = (req, res) => {
-    res.send('OLA BOLA')
-}
+import bcrypt from 'bcryptjs';
+import User from '../models/user.model.js'
 
-export const updateUserInfo = (req, res, next) => {
+export const updateUserInfo = async (req, res, next) => {
 
-    if(req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own account') )
+    if (req.user.id !== req.params.id) return next(errorHandler(401, 'You can only update your own account'))
+    
+    try {
+
+        if (req.user.password) {
+            req.body.password = bcrypt.hashSync(req.body.password, 10);
+        }
+
+        const updateUser = await User.findByIdAndUpdate(req.params.id, {
+            
+        })
+    } catch (error) {
+        next(error);
+    }
 }
