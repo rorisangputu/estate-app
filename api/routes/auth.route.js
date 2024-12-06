e577397d93824a8cf4bb70650cbfb9897bd6b3e7
Rorisang Putu <rorisangputu@gmail.com>
2024-12-06 03:10:45 +0200

    adding google api route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { signIn, signUp } from "../controllers/auth.controller.js";
+import { signIn, signUp, google } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post('/sign-up', signUp)
 router.post('/sign-in', signIn)
+router.post('/google', google)
 
 export default router;
