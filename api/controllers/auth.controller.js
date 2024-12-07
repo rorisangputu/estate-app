75052d0955e610ac00567f2f2664c6d42bca1fd8
Rorisang Putu <rorisangputu@gmail.com>
2024-12-07 17:18:33 +0200

    creating api function to signout user

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -49,12 +49,22 @@ export const signIn = async (req, res, next) => {
             .status(200)
             .json(userInfo);
         
-        
     } catch (error) {
         next(error);
     }
-
 }
+
+export const signOut = async (req, res, next) => {
+
+    try {
+
+        res.clearCookie('access_token');
+        res.status(200).json('User has been logged out');
+
+     } catch (error) {
+        next(error)
+    }
+ }
 
 export const google = async (req, res, next) => {
     try {
