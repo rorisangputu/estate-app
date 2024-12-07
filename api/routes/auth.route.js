49408f681c066ad76490fb74f2e5eb9c3f11d81c
Rorisang Putu <rorisangputu@gmail.com>
2024-12-07 16:59:16 +0200

    addinf sign out route

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,10 +1,11 @@
 import express, { Router } from "express";
-import { signIn, signUp, google } from "../controllers/auth.controller.js";
+import { signIn, signUp, google, signOut } from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post('/sign-up', signUp)
 router.post('/sign-in', signIn)
+router.get('/sign-out', signOut)
 router.post('/google', google)
 
 export default router;
