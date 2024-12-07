21934d0324a8f6105075b71548b0874d4ea3a8f2
Rorisang Putu <rorisangputu@gmail.com>
2024-12-07 17:53:00 +0200

    listing functions

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,13 @@
-export const createListing = (req, res, next) => {
-    
+
+export const listings = async (req, res, next) => {
+
+}
+
+export const createListing = async (req, res, next) => {
+
+    try {
+        
+    } catch (error) {
+        next(error)
+    }
 }
