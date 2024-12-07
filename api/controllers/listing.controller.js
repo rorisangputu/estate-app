17ff7a514aade0dfc5b984c41f95fb355ee3e5fa
Rorisang Putu <rorisangputu@gmail.com>
2024-12-07 18:56:52 +0200

    create listing func

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,3 +1,4 @@
+import Listing from "../models/listing.model.js";
 
 export const listings = async (req, res, next) => {
 
@@ -6,6 +7,8 @@ export const listings = async (req, res, next) => {
 export const createListing = async (req, res, next) => {
 
     try {
+        const listing = await Listing.create(req.body);
+        return res.status(201).json(listing);
         
     } catch (error) {
         next(error)
