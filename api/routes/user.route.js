ae653608d59038ebeccb57cbf0871f9eef1f8869
Rorisang Putu <rorisangputu@gmail.com>
2024-12-08 23:32:28 +0200

    creating user listing route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {  deleteUserInfo, updateUserInfo } from "../controllers/user.controller.js";
+import {  deleteUserInfo, updateUserInfo, getUserListings } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = Router();
 
 
 router.post('/update/:id', verifyToken, updateUserInfo);
 router.delete('/delete/:id', verifyToken, deleteUserInfo);
+router.get('/listings/:id', verifyToken, getUserListings)
 
 export default router;
