dd80d274d3ff03b29532bcb54604244a0a18b5df
Rorisang Putu <rorisangputu@gmail.com>
2024-12-09 15:22:02 +0200

    deleting listing and sending a message back

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -21,9 +21,15 @@ export const deleteListing = async (req, res, next) => {
 
     if (!listing) return next(errorHandler(404, 'Listing not found'));
 
-    if (req.user.id !== listing.userRef.toString())
+    if (req.user.id !== listing.userRef)
         return next(errorHandler(401, 'You can only delete your own listing'));
 
-    
+    try {
+        await Listing.findByIdAndDelete(req.params.id);
+        res.status(200).json({ message: 'Listing has been deleted' });
+    } catch (error) {
+        next(error);
+    }
+
 
 }
