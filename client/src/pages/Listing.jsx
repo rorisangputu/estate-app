ec7a2739df1fe4f23547000b60ebf3a59cd61f79
Rorisang Putu <rorisangputu@gmail.com>
2024-12-09 20:34:00 +0200

    fetching listing data

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 
 const Listing = () => {
+    const [listing, setListing] = useState({})
+    const params = useParams();
+
+    useEffect(() => {
+        const fetchListing = async () => {
+            const listingId = params.id;
+            const res = await fetch(`/api/listing/get/${listingId}`)
+            const data = await res.json()
+
+            if (data.success === false) {
+                console.log(data.message);
+            }
+            setListing(data);
+            //console.log(listing);
+        }
+
+        fetchListing();
+    }, []);
     return (
         <div>Listing</div>
     )
