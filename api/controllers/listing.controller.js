90d8fe98fd38b7b10127f4d24fb72c46c405acb8
Rorisang Putu <rorisangputu@gmail.com>
2024-12-09 16:46:06 +0200

    completing update function

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -41,6 +41,15 @@ export const editListing = async (req, res, next) => {
     if (req.user.id !== listing.userRef)
         return next(errorHandler(401, 'You can only edit your own listings'))
 
-    
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            { new: true }
+        );
+        res.status(200).json(updatedListing);
+    } catch (error) {
+       next(error) 
+    }
 
 }
