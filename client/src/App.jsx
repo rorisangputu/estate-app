65e9a10d824c5e423175afe60e3da38325d4deed
Rorisang Putu <rorisangputu@gmail.com>
2024-12-09 20:33:45 +0200

    adding listing page to app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css'
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../src/pages/Home'
 import SignIn from './pages/SignIn';
@@ -9,6 +9,7 @@ import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing';
+import Listing from './pages/Listing';
 function App() {
 
 
@@ -19,12 +20,14 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='/sign-up' element={<SignOut />} />
+
         <Route element={<PrivateRoute />}>
           <Route path='/profile' element={<Profile />} />
           <Route path='/create-listing' element={<CreateListing />} />
           <Route path='/update-listing/:id' element={<UpdateListing />} />
         </Route>
         <Route path='/about' element={<About />} />
+        <Route path='/listing/:id' element={<Listing />} />
       </Routes>
     </BrowserRouter>
   )
