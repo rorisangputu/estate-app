8b1c08e764ab85ebf77dc346428a16e6a95d7837
Rorisang Putu <rorisangputu@gmail.com>
2024-12-10 23:44:19 +0200

    fetching landlord from db

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -7,7 +7,20 @@ const Contact = ({ listing }) => {
     const [landlord, setLandlord] = useState(null);
 
     useEffect(() => {
+        const fetchLandlord = async () => {
+            try {
+                const res = await fetch(`/api/user/${listing.userRef}`);
+                const data = await res.json();
+                if (data.success === false) {
+                    return;
 
+                }
+                setLandlord(data);
+            } catch (error) {
+                console.log(error);
+
+            }
+        }
     }, [listing.userRef]);
 
     return (
