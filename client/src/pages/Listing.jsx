2c01383dbdc4755e05e16f98c22f9d57c4529567
Rorisang Putu <rorisangputu@gmail.com>
2024-12-10 12:04:56 +0200

    importing swiper css

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import { Navigation } from 'swiper/modules';
-
+import 'swiper/swiper-bundle.css';
 const Listing = () => {
     const [listing, setListing] = useState({})
     const [loading, setLoading] = useState(true);
