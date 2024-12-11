472f7640a0bc97852ceb1ac4ebc97c0db2891bdc
Rorisang Putu <rorisangputu@gmail.com>
2024-12-11 17:52:55 +0200

    adding search query logic for furnished

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -71,10 +71,15 @@ export const getListings = async (req, res, next) => {
     try {
         const limit = parseInt(req.query.limit) || 9;
         const startIndex = parseInt(req.query.startIndex) || 0;
+        
         let offer = req.query.offer;
-
         if (offer === 'undefined' || offer === 'false') {
             offer = { $in: [false, true] };
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === 'undefined' || furnished === 'false') {
+            furnished = { $in: [false, true] };  
         }
 
     } catch (error) {
