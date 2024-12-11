5ba102fdd102e98ea4d6ce3f130be3d8b106b9b3
Rorisang Putu <rorisangputu@gmail.com>
2024-12-11 22:00:35 +0200

    styling form

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -4,7 +4,7 @@ const Search = () => {
     return (
         <div className="flex flex-col md:flex-row">
             <div className="p-7  border border-b-2 md:border-r-2 md:min-h-screen">
-                <form action="">
+                <form action="" className="flex flex-col gap-8">
                     <div className="flex items-center gap-2">
                         <label className="whitespace-nowrap">Search:</label>
                         <input type="text" name=""
