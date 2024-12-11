07b9ca8640cd59934b353384fea2079f6af758ba
Rorisang Putu <rorisangputu@gmail.com>
2024-12-11 17:59:57 +0200

    search logic for type

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -87,6 +87,11 @@ export const getListings = async (req, res, next) => {
             parking = { $in: [false, true] };
         }
 
+        let type = req.query.type;
+        if (type === 'undefined' || type === 'all') {
+            type = { $in: ['sale', 'rent'] };
+        }
+
     } catch (error) {
         next(error)
     }
