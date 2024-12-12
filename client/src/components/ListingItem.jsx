d58b96bf34d057b5a185178cf5f337bc70c4a29e
Rorisang Putu <rorisangputu@gmail.com>
2024-12-12 16:35:30 +0200

    adding styles to listing info

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -11,14 +11,15 @@ const ListingItem = ({ listing }) => {
                     className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105
                     transition-scale duration-300 "
                 />
-                <div className="p-3">
+                <div className="p-3 flex flex-col gap-2 w-full">
                     <p className="text-lg font-semibold 
                         text-slate-700 truncate"
                     >
                         {listing.name}
                     </p>
-                    <div>
-                        <MdLocationOn />
+                    <div className="flex items-center gap-2">
+                        <MdLocationOn className="h-5 w-5 text-green-600" />
+                        <p className="truncate text-sm text-gray-600">{listing.address}</p>
                     </div>
                 </div>
             </Link>
