0938085e5dda234ef4e928c94dcf46652fae6f7c
Rorisang Putu <rorisangputu@gmail.com>
2024-12-12 16:23:30 +0200

    styling card

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -4,12 +4,15 @@ import { Link } from "react-router-dom"
 
 const ListingItem = ({ listing }) => {
     return (
-        <div>
+        <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg">
             <Link to={`/listing/${listing._id}`}>
                 <img src={listing.imageUrls[0]} alt="listing cover"
                     className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105
                     transition-scale duration-300 "
                 />
+                <div>
+                    <p>{listing.name}</p>
+                </div>
             </Link>
         </div>
     )
