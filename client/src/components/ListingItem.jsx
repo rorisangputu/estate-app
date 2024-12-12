7a402f80735fb517eaffffa6cdf67b22ffd27a17
Rorisang Putu <rorisangputu@gmail.com>
2024-12-12 16:28:25 +0200

    adding width to listing item

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -4,7 +4,8 @@ import { Link } from "react-router-dom"
 
 const ListingItem = ({ listing }) => {
     return (
-        <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg">
+        <div className="bg-white shadow-md hover:shadow-lg transition-shadow 
+        overflow-hidden rounded-lg w-full sm:w-[330px]">
             <Link to={`/listing/${listing._id}`} className="flex flex-col gap-4 ">
                 <img src={listing.imageUrls[0]} alt="listing cover"
                     className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105
