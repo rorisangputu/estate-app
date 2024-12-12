d819b8ce95cb57e195fff825823768c5e0fdd8d6
Rorisang Putu <rorisangputu@gmail.com>
2024-12-12 16:21:04 +0200

    styling image

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -6,7 +6,10 @@ const ListingItem = ({ listing }) => {
     return (
         <div>
             <Link to={`/listing/${listing._id}`}>
-                <img src={listing.imageUrls[0]} alt="" />
+                <img src={listing.imageUrls[0]} alt="listing cover"
+                    className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105
+                    transition-scale duration-300 "
+                />
             </Link>
         </div>
     )
