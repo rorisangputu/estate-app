d5453f4905d879948aa684922e7593554453333b
Rorisang Putu <rorisangputu@gmail.com>
2024-12-12 16:02:44 +0200

    adding link component to wrap listing item and displaying first image in listing

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -1,8 +1,14 @@
+/* eslint-disable react/prop-types */
+import { Link } from "react-router-dom"
 
 
 const ListingItem = ({ listing }) => {
     return (
-        <div>{listing.name}</div>
+        <div>
+            <Link to={`/listing/${listing._id}`}>
+                <img src={listing.imageUrls[0]} alt="" />
+            </Link>
+        </div>
     )
 }
 
